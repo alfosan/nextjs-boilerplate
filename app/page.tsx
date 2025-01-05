e6fa06c40f6121e82e8b7a157d14c31e432d97a4
Llorenç Alfonso Sanchis <lloalfsan@gmail.com>
2025-01-05 03:40:43 +0100

    Debugeando el error.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
